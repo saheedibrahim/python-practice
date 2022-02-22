6aae67ec2b79a54c7ba81372b9fc5f028b2675d5
saheedibrahim <saolabram@gmail.com>
2022-02-22 02:16:50 +0100

    update week 4 assignents

--- week 4/journal_unit4_2.py
+++ week 4/journal_unit4_2.py
@@ -1,15 +1,11 @@
 import math
 def volume_cylinder(r = 'radius', h = 'height'):
     if r < 0 or h < 0:                      #test to make sure te input is positive integer
-        print('Only positive integer is allowed for both values')
         return None
-#     else:
-#         print(f'radius = {r}, height = {h}')
-#         volume = math.pi * r**2 * h         #calculating volume of cylinder in cm3
-#         print(f'volume of the cylinder is {volume}')
-#         return volume
+    else:
+        volume = math.pi * r**2 * h         #calculating volume of cylinder in cm3
+        return volume
         
-volume_cylinder(-3, 4)
-# print(volume_cylinder(5, 6))
-# print(volume_cylinder(3, 7))
-# print(volume_cylinder(7, 20))
+print(volume_cylinder(5, 6))
+print(volume_cylinder(3, 7))
+print(volume_cylinder(7, 20))
